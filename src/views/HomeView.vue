903874d8aab3a3fd4591f56936931af9d825e141
yshdd <ydk21732@gmail.com>
2022-10-18 17:05:31 +0900

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Django REST Framework+Vueの開発テスト
+            </p>
+            <p class="subtitle">
+                ECサイト風のwebアプリケーション
+            </p>
+        </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered">Books of programming</h2>
+      </div>
+
+      <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail">
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name}}</h3>
+          <p class="is-size-6 has-text-gray">{{ product.price }}</p>
+
+          <RouterLink v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</RouterLink>
+        </div>
+      </div>
+    </div>
+    </div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
+  data(){
+    return {
+      latestProducts: []
+    }
+  },
   components: {
-    HelloWorld
+    
+  },
+  components:{
+
+  },
+  mounted(){
+    this.getLatestProducts()
+  },
+  methods:{
+    getLatestProducts(){
+      axios
+      .get('/api/v1/latest-products')
+      .then(response =>{
+        this.latestProducts = response.data
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+    }
   }
 }
 </script>
+
+<!-- mounted: このページの処理が終了した後に呼び出される -->
+
+<style scoped>
+.image{
+  margin: top -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
